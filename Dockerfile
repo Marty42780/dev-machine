768910969a7415d1debd1fff35dcf20770c2c4dc
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2023-01-24 15:24:52 +0100

    added workflows

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,9 @@
 FROM ubuntu
 COPY init.sh /
 RUN chmod +x /init.sh
-RUN apt-get update
-RUN apt-get install -y openssh-server
-RUN apt-get install -y git
-RUN apt-get install -y npm
-RUN apt-get install -y zsh
-RUN apt-get install -y tmux
-RUN apt-get install -y curl
-RUN apt-get install -y vim
+RUN  apt-get update \
+  && apt-get install -y openssh-server git npm zsh tmux curl vim
 RUN npm install --global http-server
 RUN service ssh start
 EXPOSE 22
-
 CMD [ "/init.sh" ]
--- init.sh
+++ init.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 # Autorize root ssh connection
-echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 
 # Download and install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Download Plug
+# Download Vim-Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Download vim Plugins
+# Download Vim Plugins
 sh -c "$(curl -fsSL $plugdl)" ""
 
 # Download zshrc and vimrc dot files from web
@@ -26,5 +25,3 @@ http-server ~/workspace -p 44000 -c-1 &
 
 # Wait for any process to exit
 wait -n
-
-
